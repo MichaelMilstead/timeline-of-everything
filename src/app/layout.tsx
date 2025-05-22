6a87415b263dfedc56c58f7a90f8dbda946f9173
michael <mike.michaelmilstead@gmail.com>
2025-05-21 17:39:56 -0700

    add page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-"use client";
-import { components } from "@/lib/tambo";
-import { TamboProvider } from "@tambo-ai/react";
 import { Gelasio } from "next/font/google";
 import "./globals.css";
 
 const gelasio = Gelasio({
   subsets: ["latin"],
 });
+
+export const metadata = {
+  title: "Timeline of Everything",
+};
 
 export default function RootLayout({
   children,
@@ -15,15 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${gelasio.className} antialiased`}>
-        <TamboProvider
-          apiKey={process.env.NEXT_PUBLIC_TAMBO_API_KEY!}
-          components={components}
-          tamboUrl={process.env.NEXT_PUBLIC_TAMBO_API_URL}
-        >
-          {children}
-        </TamboProvider>
-      </body>
+      <body className={`${gelasio.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
+"use client";
 import { TimelineSpace } from "@/components/timeline-space";
 import { MessageThreadFull } from "@/components/ui/message-thread-full";
+import { components } from "@/lib/tambo";
+import { TamboProvider } from "@tambo-ai/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 flex flex-col font-gelasio">
-      <main className="flex-1 w-full mx-auto flex flex-col items-center">
-        <h1 className="text-2xl font-bold py-4">Timeline of Everything</h1>
-        <div className="flex-1 flex w-[90%] mx-auto flex-col items-center justify-center h-full">
-          <TimelineSpace />
-        </div>
-        <div className="w-full max-w-4xl mx-auto">
-          <MessageThreadFull contextKey="tambo-template" />
-        </div>
-      </main>
-    </div>
+    <TamboProvider
+      apiKey={process.env.NEXT_PUBLIC_TAMBO_API_KEY!}
+      components={components}
+      tamboUrl={process.env.NEXT_PUBLIC_TAMBO_API_URL}
+    >
+      <div className="min-h-screen p-8 flex flex-col font-gelasio">
+        <main className="flex-1 w-full mx-auto flex flex-col items-center">
+          <h1 className="text-2xl font-bold py-4">Timeline of Everything</h1>
+          <div className="flex-1 flex w-[90%] mx-auto flex-col items-center justify-center h-full">
+            <TimelineSpace />
+          </div>
+          <div className="w-full max-w-4xl mx-auto">
+            <MessageThreadFull contextKey="tambo-template" />
+          </div>
+        </main>
+      </div>
+    </TamboProvider>
   );
 }
